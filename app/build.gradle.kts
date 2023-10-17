aaa50c7cbf5bdc9d413f085d76465fb77db1aca9
AureliusLim <aurelius_justin_lim@dlsu.edu.ph>
2023-10-17 11:45:08 +0800

    draw routes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
     }
 }
 
+
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -44,7 +46,8 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.google.android.gms:play-services-location:18.0.0")
-
+    implementation("com.google.android.libraries.places:places:3.2.0")
+    implementation ("com.github.malikdawar:drawroute:1.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/mobdeves14/cadaolimyongco/MainActivity.kt
+++ app/src/main/java/com/mobdeves14/cadaolimyongco/MainActivity.kt
@@ -17,6 +17,18 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import android.util.Log
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.api.model.Place.Field
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import com.maps.route.extensions.drawRouteOnMap
+import com.maps.route.extensions.moveCameraOnMap
+import com.maps.route.model.TravelMode
+
 
 /**
  * An activity that displays a Google map with a marker (pin) to indicate a particular location.
@@ -25,7 +37,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
     private lateinit var mMap: GoogleMap
     private lateinit var lastLocation:Location
     private lateinit var fusedLocationClient : FusedLocationProviderClient
+    private lateinit var currentloc : LatLng
 
+    val apiKey = "AIzaSyDm7Z2QpveiwSsWmh4Vr7iFfD_pepJIFtc"
     companion object{
         private const val LOCATION_REQUEST_CODE = 1
         private const val TAG = "MainActivity"
@@ -39,7 +53,37 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Initialize Places API
+        Places.initialize(applicationContext, apiKey) // Replace with your API key
+        var placesClient = Places.createClient(this)
+
+
+
     }
+    fun getUrl(origin: LatLng, destination: LatLng, travelMode: String): String {
+        val strOrigin = "origin=${origin.latitude},${origin.longitude}"
+        val strDestination = "destination=${destination.latitude},${destination.longitude}"
+        val mode = "mode=$travelMode"
+
+        return "https://maps.googleapis.com/maps/api/directions/json?$strOrigin&$strDestination&$mode&key=AIzaSyDm7Z2QpveiwSsWmh4Vr7iFfD_pepJIFtc"
+    }
+    fun drawRouteToDestination(googleMap: GoogleMap, source: LatLng, destination: LatLng){
+
+        googleMap?.run {
+            moveCameraOnMap(latLng = source) // if you want to zoom the map to any point
+            googleMap.clear()
+            //Called the drawRouteOnMap extension to draw the polyline/route on google maps
+            drawRouteOnMap(
+                "AIzaSyDm7Z2QpveiwSsWmh4Vr7iFfD_pepJIFtc", //your API key
+                source = source, // Source from where you want to draw path
+                destination = destination, // destination to where you want to draw path
+                context = this@MainActivity, // Activity Context
+                travelMode = com.maps.route.model.TravelMode.WALKING//Travel mode, by default it is DRIVING
+            )
+        }
+    }
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
@@ -47,6 +91,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
         mMap.uiSettings.isZoomControlsEnabled = true
 
         setUpMap()
+        // Initialize Autocomplete fragment for place selection
+        val autocompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+        autocompleteFragment.setPlaceFields(listOf(Field.ID, Field.NAME, Field.LAT_LNG))
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // Handle place selection
+                val destinationLatLng = place.latLng
+                if (destinationLatLng != null) {
+                    Log.d(TAG, destinationLatLng.toString())
+
+                    // Draw route to the selected destination
+                    drawRouteToDestination(googleMap,currentloc,destinationLatLng)
+                }
+            }
+
+            override fun onError(status: Status) {
+                // Handle errors
+                Log.e(TAG, "Error: ${status.statusMessage}")
+            }
+        })
     }
    private fun setUpMap(){
        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -63,15 +128,17 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
                val currentLatLong = LatLng(location.latitude, location.longitude)
                placeMarkerOnMap(currentLatLong)
                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
+               currentloc = currentLatLong
            }
        }
+
    }
 
     private fun placeMarkerOnMap(currentLatLong: LatLng){
-        val markerOptions = MarkerOptions().position(currentLatLong)
-        markerOptions.title("$currentLatLong")
-        mMap.addMarker(markerOptions)
-        Log.d(TAG, currentLatLong.toString())
+//        val markerOptions = MarkerOptions().position(currentLatLong)
+//        markerOptions.title("$currentLatLong")
+//        mMap.addMarker(markerOptions)
+//        Log.d(TAG, currentLatLong.toString())
     }
 
     override fun onMapClick(p0: LatLng) {
