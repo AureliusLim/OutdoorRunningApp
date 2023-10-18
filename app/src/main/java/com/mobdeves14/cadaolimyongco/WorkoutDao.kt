629d577f6f2a48ca92476dba3f40206323eee1b3
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2023-10-18 14:16:08 +0800

    Added WorkoutDao

---
+++ app/src/main/java/com/mobdeves14/cadaolimyongco/WorkoutDao.kt
@@ -0,0 +1,18 @@
+package com.mobdeves14.cadaolimyongco
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WorkoutDao {
+    // TODO("Add a feature that sorts by date in descending order"
+    @Query("SELECT * FROM workout")
+    fun getWorkouts(): Flow<List<Workout>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(workout: Workout)
+
+}
