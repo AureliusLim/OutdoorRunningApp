6c6e4ea2ada18e6314e0ba67b577011d985e3d73
AureliusLim <aurelius_justin_lim@dlsu.edu.ph>
2023-10-19 22:17:18 +0800

    fix db

--- app/src/main/java/com/mobdeves14/cadaolimyongco/DataGenerator.kt
+++ app/src/main/java/com/mobdeves14/cadaolimyongco/DataGenerator.kt
@@ -12,11 +12,11 @@ class DataGenerator {
 
 
 
-            data.add(WorkoutModel(23, 55, 6.9, 120, 800, "Saturday", "19", "Thursday, Oct 19 2023"))
-            data.add(WorkoutModel(14, 11, 9.0, 180, 550, "Saturday", "20", "Friday, Oct 20 2023"))
-            data.add(WorkoutModel(11, 22, 8.9, 170, 342, "Saturday", "21", "Saturday, Oct 21 2023"))
-            data.add(WorkoutModel(23, 33, 7.8, 170, 690, "Saturday",  "22", "Sunday Oct 22 2023"))
-            data.add(WorkoutModel(22, 44, 6.5, 150, 890, "Saturday", "23", "Monday, Oct 23 2023"))
+            data.add(WorkoutModel(23, 55, 6.9, 120, 800, "Thurs", "19", "Thursday, Oct 19 2023"))
+            data.add(WorkoutModel(14, 11, 9.0, 180, 550, "Fri", "20", "Friday, Oct 20 2023"))
+            data.add(WorkoutModel(11, 22, 8.9, 170, 342, "Sat", "21", "Saturday, Oct 21 2023"))
+            data.add(WorkoutModel(23, 33, 7.8, 170, 690, "Sun",  "22", "Sunday Oct 22 2023"))
+            data.add(WorkoutModel(22, 44, 6.5, 150, 890, "Mon", "23", "Monday, Oct 23 2023"))
 
             return data
         }
